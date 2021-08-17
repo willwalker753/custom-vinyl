a15e04b372bc9ba40bdfc9fa5d2b2ad7d0b10fa9
Will Walker <williamwalker753@gmail.com>
2021-08-17 02:08:28 -0500

    added dynamic importing for images from folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import img1 from "./images/1.jpg";
+import { importAll } from "./util/functions";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: null
+    }
+  }
+
+  componentDidMount() {
+    const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
+    const imagesArr = [];
+
+    for(let key in images) {
+      imagesArr.push(images[`${key}`].default)
+    }
+
+    console.log(images, imagesArr)
+    this.setState({ images: imagesArr })
+  }
+
+  render() {
+    const { images } = this.state;
+    if(images === null) {
+      return <h1>loading...</h1>
+    }
+    else {
+      return (
+        <>
+          {/* <img src={img1}></img> */}
+          {images.map((image, key) => {
+            return <img key={key} src={image}></img>;
+          })}
+          
+        </>
+      );
+    }
+    
+  }
 }
 
 export default App;
